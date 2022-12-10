d4bd91b2a2e4ec76c2f89b95e49532425f146ddc
Aydin <trabzonmuhammet@gmail.com>
2022-12-10 16:50:04 +0200

    Set.up Created gitignore

---
+++ EU10_Group_28/JAVA_coding/src/day_37_OOP_Inheritance_Continue_SuperKeyword/Dogs.java
@@ -0,0 +1,19 @@
+package day_37_OOP_Inheritance_Continue_SuperKeyword;
+
+
+
+public class Dogs {
+
+    public class Dog extends Animals {
+
+        public Dog(String name, char gender, String size, int age, String colour) {
+            super(name, gender, size, age, colour);
+
+        }
+        public void bark() {
+
+        }
+
+
+    }
+}
