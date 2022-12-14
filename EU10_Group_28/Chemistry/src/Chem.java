b0157f64e2a1dedf2b8376b84d1b0c3fa3c9a14e
Aydin <trabzonmuhammet@gmail.com>
2022-12-14 19:11:51 +0200

    new codes were added to platform

---
+++ EU10_Group_28/Chemistry/src/Chem.java
@@ -0,0 +1,5 @@
+public class Chem {
+    public static void main(String[] args) {
+        System.out.println("chemistry is fun");
+    }
+}
---
+++ EU10_Group_28/Chemistry/src/org_chem.java
@@ -0,0 +1,5 @@
+public class org_chem {
+    public static void main(String[] args) {
+        System.out.println("Organic chem");
+    }
+}
